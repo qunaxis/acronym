3933d96117a257033b4dd7da5f7db9c2b2d153c6
Dmitry <qunaxis@gmail.com>
2020-11-14 01:37:45 +0300

    fix part owl

--- app/js/app.js
+++ app/js/app.js
@@ -4,86 +4,98 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// onClick .main-content__button
 	// code
+	
+	$(window).on('resize touchmove', function () {
+		// adjust heights basing on window.innerHeight
+		// $(window).height() will not return correct value before resizing is done
+		windowHeight = $(window).height();
 
-	windowHeight = $(window).height()
+		getStickyScrollbar();
+		processParams = calcProcessParams();
+		startGsapProcess();
 
-	$(window).scroll(function() {
-		var header = $('header');
-		var scroll = $(window).scrollTop();
-		if (scroll > windowHeight * 0.85) {
-			header.fadeIn(300);
-		} else {
-			header.fadeOut(300);
-
-		}
 	});
+	
+	function getStickyScrollbar() {
+		$(window).scroll(function() {
+		var header = $('header');
+			var scroll = $(window).scrollTop();
+			if (scroll > windowHeight * 0.85) {
+				header.fadeIn(300);
+			} else {
+				header.fadeOut(300);
+			}
+		});
+	}
 
-	var owlParams = {
-		partners: {
-			autoplay: true,
-			autoplayTimeout: 4000,
-			autoplayHoverPause: true,
-			loop: true,
-			nav: false,
-			stagePadding: 0,
-			dots: false,
-			smartSpeed: 1500,
-			lazyLoad: true,
-			// center: true,
-			autoWidth: true,
-			responsive: {
-				0: {
-					items: 1,
-					margin: 40,
-				},
-				768: {
-					items: 4,
-					margin: 80,
-				},
-				992: {
-					items : 5,
-					margin: 80,
-				},
-				1200: {
-					margin: 80,
-					items : 6,
-				}
-			}			
-		},
-		team: {
-			autoplay: true,
-			autoplayTimeout: 4000,
-			autoplayHoverPause: true,
-			loop: true,
-			nav: false,
-			stagePadding: 0,
-			dots: false,
-			smartSpeed: 1500,
-			// center: true,
-			autoWidth: true,
-			responsive: {
-				0: {
-					margin: 8,
-					items: 2,
-					autoHeight: true,
-				},
-				768: {
-					margin: 32,
-					items: 3
-				},
-				992: {
-					margin: 32,
-					items : 3
-				},
-				1200: {
-					margin: 32,
-					items : 4
+	function startOwl() {
+		var owlParams = {
+			partners: {
+				autoplay: true,
+				autoplayTimeout: 4000,
+				autoplayHoverPause: true,
+				loop: true,
+				nav: false,
+				stagePadding: 0,
+				dots: false,
+				smartSpeed: 1500,
+				lazyLoad: true,
+				// center: true,
+				autoWidth: true,
+				responsive: {
+					0: {
+						items: 1,
+						margin: 40,
+					},
+					768: {
+						items: 4,
+						margin: 80,
+					},
+					992: {
+						items : 5,
+						margin: 80,
+					},
+					1200: {
+						margin: 80,
+						items : 6,
+					}
+				}			
+			},
+			team: {
+				autoplay: true,
+				autoplayTimeout: 4000,
+				autoplayHoverPause: true,
+				loop: true,
+				nav: false,
+				stagePadding: 0,
+				dots: false,
+				smartSpeed: 1500,
+				// center: true,
+				autoWidth: true,
+				responsive: {
+					0: {
+						margin: 8,
+						items: 2,
+						autoHeight: true,
+					},
+					768: {
+						margin: 32,
+						items: 3
+					},
+					992: {
+						margin: 32,
+						items : 3
+					},
+					1200: {
+						margin: 32,
+						items : 4
+					}
 				}
 			}
 		}
+		$('.owl-partners').owlCarousel(owlParams.partners);
+		$('.owl-team').owlCarousel(owlParams.team);
 	}
-	$('.owl-partners').owlCarousel(owlParams.partners);
-	$('.owl-team').owlCarousel(owlParams.team);
 
 	$(".input-effect input").val("");
 	var phoneMask = IMask(
@@ -94,13 +106,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	area = $('.form-textarea')
 	lines = 6 // Ограничение по кол-ву строк (не даст натыкать Enter)
 	area.keydown(function(e) {
-        linesCount = $(this).val().split("\n").length;
-        
+        linesCount = $(this).val().split("\n").length;        
         if(e.keyCode == 13 && linesCount >= lines) {
             return false;
         }
-        else {
-            
+        else {            
         }
     });
 
@@ -115,15 +125,24 @@ document.addEventListener("DOMContentLoaded", function() {
 	var controller = new ScrollMagic.Controller();
 
 	// var tweenArray = [];
-	cardCount = 5;
-	processSection = $('.process');
-	processOffset = processSection.offset().top;
-	header = $('header');
-	headerOffset = processSection.offset().top;
-	console.log(processOffset);
-	$('.process-wrapper').height((windowHeight - headerOffset) * cardCount)
+
+	function calcProcessParams(){
+		cardCount = 5;
+		processSection = $('.process');
+		processOffset = processSection.offset().top;
+		header = $('header');
+		headerOffset = processSection.offset().top;
+		console.log(processOffset);
+		$('.process-wrapper').height((windowHeight - headerOffset) * cardCount)
+		return {
+			processOffset,
+			headerOffset
+		}
+	}
 	
-	function startProcess() {
+	function startGsapProcess(params) {
+		var processOffset = params.processOffset
+		var headerOffset = params.headerOffset
 		for (let i = 1; i <= cardCount; i++) { // выведет 0, затем 1, затем 2
 			// var showOffset = processOffset + headerOffset + (i-1)*windowHeight - windowHeight * 0.30;
 			// var hideOffset =  processOffset + headerOffset + (i-1)*windowHeight + windowHeight * 0.05;
@@ -155,5 +174,4 @@ document.addEventListener("DOMContentLoaded", function() {
 						.addTo(controller);
 		}
 	}
-	setTimeout(1000, startProcess);
 });
